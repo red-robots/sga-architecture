25e3f4af9c2645575df8cf3899c8eddbdee0f4f8
lisadebona <annalisa.quiamco@gmail.com>
2019-06-03 23:32:25 -0600

    Updated single.php

--- single.php
+++ single.php
@@ -11,17 +11,14 @@ get_header(); ?>
 
 	<div id="primary" class="full-content-area default-temp">
 		<main id="main" class="site-main clear" role="main">
-
 			<?php
 			while ( have_posts() ) : the_post();
 				get_template_part( 'template-parts/content', 'page' );
 
 			endwhile; // End of the loop.
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
