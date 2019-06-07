ef5e4bf0200d90e866bf769d15f80a1928b99ce3
lisadebona <annalisa.quiamco@gmail.com>
2019-06-06 21:14:52 -0600

    styling news page and single news page

--- single.php
+++ single.php
@@ -9,16 +9,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="full-content-area default-temp">
-		<main id="main" class="site-main clear" role="main">
-			<?php
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/content', 'page' );
-
-			endwhile; // End of the loop.
+<div id="primary" class="full-content-area margintop portfolio-content">
+	<main id="main" class="site-main clear" role="main">
+		<div class="col-left">
+			<?php  
+			$year = get_the_date('Y');
+			$year_archives = get_site_url().'/'.$year.'/';
 			?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<div class="head1 single-title">
+				<a href="<?php echo get_site_url() ?>/news/">News</a> <span class="sep">&ndash;</span>
+				<a href="<?php echo $year_archives; ?>"><?php echo $year ?></a> <span class="sep">&ndash;</span>
+				<span class="current"><?php echo get_the_title(); ?></span>	
+			</div>
+			<?php get_template_part('template-parts/news-categories'); ?>
+		</div>
+		<div class="col-right">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php  
+					$postId = get_the_ID();
+					$thumbnail_id = get_post_thumbnail_id( $postId );
+					$pImg = wp_get_attachment_image_src($thumbnail_id,'medium_large');
+				?>
+
+				<?php if ( $pImg ) { ?>
+				<div class="single-post-Image"><?php the_post_thumbnail(); ?></div>
+				<?php } ?>
+
+				<div class="post-content">
+					<header class="post-header"><h1><?php the_title(); ?></h1> <span class="post-date">(<?php echo get_the_date('m/d/Y'); ?>)</span></header>
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile; ?>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
--- template-parts/content-news.php
+++ template-parts/content-news.php
@@ -72,14 +72,14 @@ if ( $theposts->have_posts() ) {  ?>
 		$content = strip_shortcodes($latest_content);
 		$content = preg_replace("/<img[^>]+\>/i", " ", $latest_content);          
 		$content = str_replace(']]>', ']]>', $content);
-		$excerpt = ($content) ? shortenText($content,300) : '';
+		$excerpt = ($content) ? shortenText($content,520) : '';
 		$latest_post_link = get_permalink($latest_post_id);
 		?>
 		<?php if ($paged<2) { ?>
 			<?php if( !is_archive() ) { ?>
 			<div class="lastest_post_image clear">
 				<?php if ($img) { ?>
-				<img class="featImg" src="<?php echo $img[0] ?>" alt="<?php echo $latest_post_title;?>" />		
+					<div class="featImage" style="background-image:url('<?php echo $img[0] ?>');"><img style="display:none;" src="<?php echo $img[0] ?>" alt="<?php echo $latest_post_title;?>" /></div>
 				<?php } else { ?>
 				<div class="placeholder" style="background-image:url('<?php echo $placeholderImage ?>')"></div>
 				<?php } ?>
@@ -99,39 +99,38 @@ if ( $theposts->have_posts() ) {  ?>
 
 	<div class="post-items-wrap clear">
 		<div class="itemrows clear">
-		<?php while ( $theposts->have_posts() ) : $theposts->the_post();  ?>
-			<?php  
-			$postId = get_the_ID();
-			$thumbnail_id = get_post_thumbnail_id( $postId );
-			$pImg = wp_get_attachment_image_src($thumbnail_id,'medium_large');
-			$placeholderImage2 = get_bloginfo('template_url').'/images/coming-soon.jpg'; 
-			$pImageSrc = ($pImg) ? $pImg[0] : $placeholderImage2;
-			$pcontent = get_the_content();
-			$pcontent = strip_shortcodes($pcontent);
-			$pcontent = strip_tags($pcontent);
-			$pcontent = preg_replace("/<img[^>]+\>/i", " ", $pcontent);          
-			$pcontent = str_replace(']]>', ']]>', $pcontent);
-			$pcontent = preg_replace("~(?:\[/?)[^/\]]+/?\]~s", '', $pcontent);
-			$pexcerpt = ($pcontent) ? shortenText($pcontent,100) : '';
-			?>
-			<a class="post-item-block" href="<?php echo get_permalink(); ?>">
-				<?php if ($pImg) { ?>
-					<span class="image clear <?php echo ($pImg) ? 'has-image': 'no-image';?>"><img class="feat" src="<?php echo $pImageSrc ?>" alt="<?php echo get_the_title() ?>" /></span>
-				<?php } ?>
-				
-				<span class="post-excerpt clear">
-					<span class="ptitle clear">
-						<span class="title"><?php echo get_the_title(); ?></span>
-						<span class="date"><?php echo get_the_date('m/d/y'); ?></span>
+			<div class="flexrow">
+			<?php while ( $theposts->have_posts() ) : $theposts->the_post();  ?>
+				<?php  
+				$postId = get_the_ID();
+				$thumbnail_id = get_post_thumbnail_id( $postId );
+				$pImg = wp_get_attachment_image_src($thumbnail_id,'medium_large');
+				$placeholderImage2 = get_bloginfo('template_url').'/images/coming-soon.jpg'; 
+				$pImageSrc = ($pImg) ? $pImg[0] : $placeholderImage2;
+				$pcontent = get_the_content();
+				$pcontent = strip_shortcodes($pcontent);
+				$pcontent = strip_tags($pcontent);
+				$pcontent = preg_replace("/<img[^>]+\>/i", " ", $pcontent);          
+				$pcontent = str_replace(']]>', ']]>', $pcontent);
+				$pcontent = preg_replace("~(?:\[/?)[^/\]]+/?\]~s", '', $pcontent);
+				$pexcerpt = ($pcontent) ? shortenText($pcontent,80) : '';
+				?>
+				<a class="post-item-block" href="<?php echo get_permalink(); ?>">
+					<span class="image clear <?php echo ($pImg) ? 'has-image': 'no-image';?>"><span class="thumb" style="background-image:url('<?php echo $pImageSrc ?>');"></span><img style="display:none;" class="feat" src="<?php echo $pImageSrc ?>" alt="<?php echo get_the_title() ?>" /></span>				
+					<span class="post-excerpt clear">
+						<span class="ptitle clear">
+							<span class="title"><?php echo get_the_title(); ?></span>
+							<span class="date"><?php echo get_the_date('m/d/y'); ?></span>
+						</span>
+						
+						<?php if ($pexcerpt) { ?>
+							<?php echo $pexcerpt; ?>
+							<span class="readmore clear">Read More &gt;</span>
+						<?php } ?>
 					</span>
-					
-					<?php if ($pexcerpt) { ?>
-						<?php echo $pexcerpt; ?>
-						<span class="readmore clear">Read More &gt;</span>
-					<?php } ?>
-				</span>
-			</a>
-		<?php endwhile; wp_reset_postdata(); ?>
+				</a>
+			<?php endwhile; wp_reset_postdata(); ?>
+			</div>
 		</div>
 
 		<?php
