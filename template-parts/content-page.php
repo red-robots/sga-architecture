634511a2dcc30c2d239acaf204085fff5416057f
Lisa <annalisa.quiamco@gmail.com>
2019-08-06 22:46:56 -0600

    Fixed flexbox issue on safari

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -18,11 +18,10 @@
 	<div class="entry-content">
 		<?php
 			the_content();
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bellaworks' ),
-				'after'  => '</div>',
-			) );
+			// wp_link_pages( array(
+			// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bellaworks' ),
+			// 	'after'  => '</div>',
+			// ) );
 		?>
 	</div><!-- .entry-content -->
 	<?php } ?>
